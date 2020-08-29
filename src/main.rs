3b4f6d7e5374e02e887323927b317fddeebba5b4
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-08-29 18:49:38 -0500

    Server able to run and respond with port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    //constructor
+    fn new(address: String) -> Self {
+        Self { address: address }
+    }
+
+    fn run(self) {
+        println!("Server up on port {}", self.address)
+    }
 }
