f8dbed34dfdb8e77eb895734a70240c231639034
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-04 00:13:26 -0500

    set up unimplemented Try_From to parse buffer

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,16 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method, //using parents mod
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buff: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -25,6 +27,11 @@ impl Server {
                         Ok(_) => {
                             //converts to text
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(err) => println!("Failed to parse a request: {}", err),
+                            }
                         }
                         Err(err) => println!("Failed to read from connection: {}", err),
                     }
